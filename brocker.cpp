5ef841cd5044d206b843fb6249c989ea2ea0e386
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2024-07-04 14:29:03 +0300

    Add files via upload
    brocker.cpp is just zmq::proxy + server + workers

---
+++ brocker.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <zmq.hpp>
+
+static int within(int range) 
+{
+    return rand() % range;
+}
+
+class ServerWorker 
+{
+public:
+    ServerWorker(zmq::context_t& context) : _workerSocket(context, zmq::socket_type::dealer) 
+    {
+        _workerSocket.connect("inproc://backend");
+    }
+
+    void operator()() 
+    {
+        while (true) 
+        {
+            zmq::message_t identity;
+            zmq::message_t request;
+
+            _workerSocket.recv(identity);
+            _workerSocket.recv(request);
+
+            std::string reply_str = "Reply: " + request.to_string();
+            zmq::message_t reply(reply_str.size());
+            memcpy(reply.data(), reply_str.data(), reply_str.size());
+
+            _workerSocket.send(identity, zmq::send_flags::sndmore);
+            _workerSocket.send(reply, zmq::send_flags::none);
+        }
+    }
+
+private:
+    zmq::socket_t _workerSocket;
+};
+
+class Server 
+{
+public:
+    Server() : _context(1), _frontendSocket(_context, zmq::socket_type::router), _backendSocket(_context, zmq::socket_type::dealer) 
+    {
+        _frontendSocket.bind("tcp://*:5555");
+        _backendSocket.bind("inproc://backend");
+    }
+
+    void Run(const uint16_t MAX_THREADS) 
+    {
+        std::vector<std::thread> workers;
+
+        for (uint16_t i = 0; i < MAX_THREADS; i++)
+        {
+            workers.emplace_back(ServerWorker(_context));
+        }
+
+        zmq::proxy(_frontendSocket, _backendSocket);
+
+        for (auto& worker : workers) 
+        {
+            worker.join();
+        }
+    }
+
+private:
+    zmq::context_t _context;
+    zmq::socket_t _frontendSocket;
+    zmq::socket_t _backendSocket;
+};
+
+int main(int argc, char** argv) 
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage:\n\tserver <threads_count>" << std::endl;
+        return 1;
+    }
+
+    srand(time(0));
+
+    Server server;
+    server.Run(atoi(argv[1]));
+    return 0;
+}
---
+++ client.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <thread>
+#include <zmq.hpp>
+#include <sstream>
+
+static int within(int range) 
+{
+    return rand() % range;
+}
+
+class Client 
+{
+public:
+    Client() : _context(1), _clientSocket(_context, zmq::socket_type::dealer) 
+    {
+        _clientSocket.connect("tcp://localhost:5555");
+
+        std::ostringstream oss;
+        oss << std::hex << within(0x10000) << "-" << within(0x10000);
+        std::string identity = oss.str();
+        _clientSocket.set(zmq::sockopt::routing_id, identity);
+    }
+
+    void Run()
+    {
+        int request_num = 0;
+
+        while (true) 
+        {
+            std::string request_str;
+            std::ostringstream oss;
+            oss << "Request #" << request_num++;
+            request_str = oss.str();
+
+            zmq::message_t request(request_str.size());
+            std::memcpy(request.data(), request_str.data(), request_str.size());
+
+            _clientSocket.send(request, zmq::send_flags::none);
+
+            zmq::message_t reply;
+            _clientSocket.recv(reply);
+
+            std::cout << "Received: " << reply.to_string() << std::endl;
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+
+private:
+    zmq::context_t _context;
+    zmq::socket_t _clientSocket;
+};
+
+int main() 
+{
+    srand(time(0));
+
+    Client client;
+    client.Run();
+
+    return 0;
+}
